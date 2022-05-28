a55a0d7d17164d59f0cd0119697b1feb569b330c
Waqas Tahir <2waqastahirciit@gmail.com>
2022-05-28 16:48:36 +0500

    multiply file added

---
+++ Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply {
+    public static void main(String args[]){
+        int a=3,b=5,multi=0;
+        multi = a*b;
+         System.out.println("The sum is:"+multi);
+
+    }
+}
--- Sum.java
+++ Sum.java
@@ -2,7 +2,7 @@ public class Sum {
     public static void main(String args[]){
         int a=3,b=5,sum=0;
         sum = a+b;
-         System.out.println(sum);
+         System.out.println("The sum is:"+sum);
 
     }
 }
